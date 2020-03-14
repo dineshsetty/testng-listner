45fd165e3571b6921a5db7a6229a653faee99d3d
Dinesh T <dineshsetty@gmail.com>
2020-03-15 00:24:55 +0530

    Proper project structure

---
+++ testng-project/src/test/java/com/dinesh/testng/TestClass2.java
@@ -0,0 +1,17 @@
+package com.dinesh.testng;
+
+import org.testng.annotations.Test;
+
+public class TestClass2 {
+	
+	@Test
+	public void testCase4() {
+		System.out.println("Test Case 4 Executed");
+	}
+	
+	@Test
+	public void testCase5() {
+		System.out.println("Test Case 5 Executed");
+	}
+
+}
