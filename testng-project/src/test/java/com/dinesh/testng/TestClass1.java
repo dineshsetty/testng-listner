c5cf0538f949bb33fd9f3ee9bd4e078db04582b6
Dinesh T <dineshsetty@gmail.com>
2020-03-16 09:44:57 +0530

    Log4j Added

--- testng-project/src/test/java/com/dinesh/testng/TestClass1.java
+++ testng-project/src/test/java/com/dinesh/testng/TestClass1.java
@@ -1,22 +1,28 @@
 package com.dinesh.testng;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 public class TestClass1 {
 	
+	private static Logger Log = Logger.getLogger(TestClass1.class.getName());
+	
 	@Test
 	public void testCase1() {
 		System.out.println("Test Case 1 Executed");
+		Log.info("Test Case 1 Executed");
 	}
 	
 	@Test
 	public void testCase2() {
 		System.out.println("Test Case 2 Executed");
+		Log.error("Test Case 2 Executed");
 	}
 
 	@Test
 	public void testCase3() {
 		System.out.println("Test Case 3 Executed");
+		Log.fatal("Test Case 3 Executed");
 	}
 
 }
